defce35e0f6e334ec819f02024367158d41e628f
Robert Hogan <robhogan@gmail.com>
2022-02-08 19:23:26 +0000

    Filter out stories more than 1 day old

--- reader.js
+++ reader.js
@@ -23,6 +23,14 @@ function makePrevNextArray(a = [], start = 0, step = 1) {
     add: function(b) {
       // Only add stories we don't already have.
       b = b.filter(i => !uniqueEntries.has(i.title));
+      // Only add stories from the last 24 hrs
+      b = b.filter(i => {
+        let d = new Date(i["dc:date"]).getTime();
+        let n = new Date().getTime();
+        // This is the number of ms in 24hrs.
+        return (n - d < 86362145);
+      });
+
       b.forEach(i => uniqueEntries.set(i.title, 1));
       a = a.concat(b);
     },
